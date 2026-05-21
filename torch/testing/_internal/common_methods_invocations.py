d57b91ae374c21853b7b69f71e12cfb572a625dc
Benjamin Glass <bglass@openteams.com>
2026-05-21 20:05:51 +0000

    Refactor CompositeExplicitAutograd for aten.native_group_norm
    Fixes issues with LazyTensor OpInfo testing, which dispatches through `CompositeExplicitAutograd` rather than `CPU` or `CUDA`. Some of the test cases have input tensors with len-0 dimensions, and `native_batch_norm` internally errors on that (even though they're valid inputs to `nn.GroupNorm`). Match the implementation more closely to the existing ref decomposition and the original group_norm paper.
    
    ghstack-source-id: ea81c53ff8c60880d8292547d70211dacd442193
    Pull-Request: https://github.com/pytorch/pytorch/pull/183001

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -15905,9 +15905,6 @@ op_db: list[OpInfo] = [
             DecorateInfo(unittest.expectedFailure, "TestConsistency", "test_output_match", device_type="mps", dtypes=(torch.int32, torch.int16, torch.int8, torch.uint8)),
             # tensor-likes are not close
             DecorateInfo(unittest.expectedFailure, "TestInductorOpInfo", "test_comprehensive", device_type="cuda", dtypes=(torch.float16,)),
-            # fails in TorchScript interpreter with failures associated with batch_norm
-            DecorateInfo(unittest.expectedFailure, "TestLazyOpInfo", "test_correctness"),
-            DecorateInfo(unittest.expectedFailure, "TestLazyOpInfo", "test_correctness_with_reusing_ir"),
             # lazy dispatch failure
             DecorateInfo(unittest.expectedFailure, "TestLazyOpInfo", "test_dispatched_to_lazy"),
             # native_group_norm expects contiguous inputs on CUDA
